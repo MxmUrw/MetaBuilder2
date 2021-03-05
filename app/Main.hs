988cad97840726bc72329d9f1a3d387fa44ba612
Maxim Urschumzew <m.urschumzew@outlook.com>
2021-03-05 19:44:17 +0100

    Add ProjectType typeclass.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,6 @@
 module Main where
 
-import Lib
+import MetaBuilder.Main
 
 main :: IO ()
 main = someFunc
--- src/MetaBuilder/Main.hs
+++ src/MetaBuilder/Main.hs
@@ -1,6 +1,11 @@
-module Lib
+module MetaBuilder.Main
     ( someFunc
     ) where
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+testf :: Int -> Int
+testf 0 = 0
+
+
